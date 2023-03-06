b7e585b5466956d86426573e8a4bb6d152dbf34c
carlosfpontes <127152247+carlosfpontes@users.noreply.github.com>
2023-03-06 18:47:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+}
+
+header h1 {
+  margin: 0;
+}
+
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+footer p {
+  margin: 0;
+}
+
+main {
+  margin: 2rem;
+}
+
+#form-container {
+  display: flex;
+  justify-content: center;
+}
+
+form {
+  width: 50%;
+  max-width: 600px;
+}
+
+#button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+}
+
+.hidden {
+  display: none;
+}
