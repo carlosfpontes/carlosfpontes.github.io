3f60a316bd949511e5503f100826aa55669804a9
carlosfpontes <127152247+carlosfpontes@users.noreply.github.com>
2023-05-10 00:30:10 -0300

    Add files via upload

---
+++ Presencial/script_pesquisa.js
@@ -0,0 +1,83 @@
+// Função de rolar a página até o topo
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Obtém todos os formulários pela ID
+var forms = [
+	document.getElementById('form1'),
+	document.getElementById('form2'),
+	document.getElementById('form3'),
+	document.getElementById('form4'),
+	document.getElementById('form5'),
+	document.getElementById('form6'),
+	document.getElementById('form7'),
+	document.getElementById('form8'),
+	document.getElementById('form9')
+];
+
+// Define uma variável para acompanhar o formulário atual
+var currentForm = 0;
+
+// Exibe o próximo formulário
+function showNextForm() {
+	// Esconde o formulário atual
+	forms[currentForm].style.display = 'none';
+
+	// Incrementa o índice do formulário atual e verifica se todos foram exibidos
+	currentForm++;
+	if (currentForm === forms.length) {
+		// Exibe a mensagem de agradecimento e o botão de retorno
+		document.getElementById('mensagem').style.display = 'block';
+		scrollToTop(); // Rolar a página para o topo
+	return;
+	}
+
+	// Atualiza o contador de formulários exibidos
+	var contador = document.getElementById('contador');
+	contador.innerHTML = 'QUESTIONÁRIO ' + (currentForm + 1) + '/' + forms.length;
+
+	// Exibe o próximo formulário
+	forms[currentForm].style.display = 'block';
+	scrollToTop(); // Rolar a página para o topo
+}
+
+// Embaralha os formulários em uma ordem aleatória
+forms.sort(function() { return 0.5 - Math.random() });
+
+// Esconde todos os formulários, exceto o primeiro
+forms.forEach(function(form, index) {
+	if (index === 0) {
+		form.style.display = 'block';
+	} else {
+		form.style.display = 'none';
+	}
+});
+
+// Adiciona um ouvinte de eventos para cada botão de envio
+forms.forEach(function(form) {
+	form.addEventListener('submit', function() {
+		// Define o atributo target do formulário para _blank
+		form.setAttribute('target', '_blank');
+
+		// Exibe o próximo formulário
+		showNextForm();
+    
+		scrollToTop(); // Rolar a página para o topo
+	});
+});
+
+// Esconde a mensagem de agradecimento e o botão de retorno inicialmente
+document.getElementById('mensagem').style.display = 'none';
+
+// Adiciona um ouvinte de eventos para o botão de retorno
+document.getElementById('botao-retorno').addEventListener('click', function() {
+	// Redireciona para a página inicial
+	window.location.href = 'index.html';
+});
+
+// Adiciona um elemento HTML para exibir o contador de formulários
+var contador = document.createElement('p');
+contador.id = 'contador';
+contador.innerHTML = 'QUESTIONÁRIO 1/' + forms.length;
+document.body.insertBefore(contador, forms[0]);
