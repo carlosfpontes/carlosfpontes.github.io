765a03fc1a45f4a32913da1e37de23929de8bdc9
carlosfpontes <127152247+carlosfpontes@users.noreply.github.com>
2024-06-14 09:21:30 -0300

    Cena 2 e 3

---
+++ panoramas/cena-02/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cena-02---vista-1",
+      "name": "Cena 02 - Vista 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.8298083403844103,
+        "pitch": 0.03204867056955685,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.242702285241613,
+          "pitch": 0.04614800177937184,
+          "rotation": 0,
+          "target": "1-cena-02---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cena-02---vista-2",
+      "name": "Cena 02 - Vista 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 1.0236485440469885,
+        "pitch": 0.024446561409702028,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2314914869226286,
+          "pitch": 0.03288778139299886,
+          "rotation": 0,
+          "target": "2-cena-02---vista-3"
+        },
+        {
+          "yaw": -1.9303452157542758,
+          "pitch": 0.045996438702282205,
+          "rotation": 0,
+          "target": "0-cena-02---vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cena-02---vista-3",
+      "name": "Cena 02 - Vista 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.4557181920796225,
+        "pitch": 0.045132113371760596,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9336193857371295,
+          "pitch": 0.055125698988966576,
+          "rotation": 0,
+          "target": "1-cena-02---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cena 02",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
---
+++ panoramas/cena-03/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cena-03---vista-1",
+      "name": "Cena 03 - Vista 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.7516376823026292,
+        "pitch": 0.046105311433381146,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2404549413437334,
+          "pitch": 0.03261841012294475,
+          "rotation": 0,
+          "target": "1-cena-03---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cena-03---vista-2",
+      "name": "Cena 03 - Vista 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.788617435551604,
+        "pitch": 0.09749107585956018,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2426593378403226,
+          "pitch": 0.026553133161939968,
+          "rotation": 0,
+          "target": "2-cena-03---vista-3"
+        },
+        {
+          "yaw": -1.93577720090682,
+          "pitch": 0.04359671016502631,
+          "rotation": 0,
+          "target": "0-cena-03---vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cena-03---vista-3",
+      "name": "Cena 03 - Vista 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.5499159812197085,
+        "pitch": 0.063937160609985,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9441097964783758,
+          "pitch": 0.03408701009270132,
+          "rotation": 0,
+          "target": "1-cena-03---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cena 03",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
