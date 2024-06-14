e8432c956d095e3d4ac1fcda09ffdfda3fd3eba6
carlosfpontes <127152247+carlosfpontes@users.noreply.github.com>
2024-06-14 09:23:49 -0300

    Cenas 4 e 5

---
+++ panoramas/cena-05/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cena-05---vista-1",
+      "name": "Cena 05 - Vista 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.4755924552712436,
+        "pitch": 0.013163533066762767,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2432879599650573,
+          "pitch": 0.04244112373005038,
+          "rotation": 0,
+          "target": "1-cena-05---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cena-05---vista-2",
+      "name": "Cena 05 - Vista 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.3727179548739592,
+        "pitch": 0.047012618095573444,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.237047768153495,
+          "pitch": 0.027553259896258453,
+          "rotation": 0,
+          "target": "2-cena-05---vista-3"
+        },
+        {
+          "yaw": -1.945175032059561,
+          "pitch": 0.035065146159102056,
+          "rotation": 0,
+          "target": "0-cena-05---vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cena-05---vista-3",
+      "name": "Cena 05 - Vista 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.3213572463507184,
+        "pitch": 0.022566056685882074,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9213156207246538,
+          "pitch": 0.06155629277793295,
+          "rotation": 0,
+          "target": "1-cena-05---vista-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cena 05",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
